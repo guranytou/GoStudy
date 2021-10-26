6855ecfd779219cfed9e2fbacb1a0dc6c3114081
guranytou <guranytou@gmail.com>
2021-10-27 00:41:52 +0900

    FizzBuzz修正

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
-	for i := 1; i < 101; i++ {
-		if i % 3 == 0 {
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 53 == 0 {
 			fmt.Println("Fizz")
-		} else if i % 5 == 0 {
+		}else if i % 5 == 0 {
 			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
