875dbb161b8a21aa9447c72c1b5e91766d4efde8
guranytou <guranytou@gmail.com>
2022-07-17 23:14:41 +0900

    add cons.go

---
+++ AtCoder/Beginners_Selection/Coins.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c, x int
+	fmt.Scanf("%d", &a)
+	fmt.Scanf("%d", &b)
+	fmt.Scanf("%d", &c)
+	fmt.Scanf("%d", &x)
+
+	count := 0
+	for i := 0; i <= a; i++ {
+		for j := 0; j <= b; j++ {
+			for k := 0; k <= c; k++ {
+				if x-500*i-100*j-50*k == 0 {
+					count++
+				}
+			}
+		}
+	}
+	fmt.Printf("%d", count)
+}
