08a4571790107da4053c5601015f78c18a4b9f04
guranytou <guranytou@gmail.com>
2023-03-15 17:30:40 +0900

    add Command line & package structure

---
+++ go-fundamentals/http-server/cmd/webserver/go.mod
@@ -0,0 +1,7 @@
+module guranytou/http-server/cmd
+
+go 1.19
+
+replace github.com/guranytou/http-server => ../../
+
+require github.com/guranytou/http-server v0.0.0-00010101000000-000000000000
--- go-fundamentals/http-server/server_test.go
+++ go-fundamentals/http-server/server_test.go
@@ -1,4 +1,4 @@
-package main
+package poker
 
 import (
 	"encoding/json"
@@ -9,25 +9,6 @@ import (
 	"reflect"
 	"testing"
 )
-
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
 
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
